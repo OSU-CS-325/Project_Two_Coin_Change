1316b84ddf71e6f72076b0e301852a8711cdf09c
ammiranda <alexandermichaelmiranda@gmail.com>
2016-10-19 22:13:10 -0700

    Progress on dp algorithm for coin change

--- dynamic-programming/changedp.py
+++ dynamic-programming/changedp.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
 
 def changedp(coinArray, change):
-    #REPLACE WITH ALGORITHM
-    coinCount = [1, 1, 1, 1]
-    totalCoins = sum(coinCount)
+  min_coins_used = [0] * (change + 1)
+  min_coins_count = [0] * (change + 1)
 
+  for c in range(change + 1):
+    coin_count = c
+    latest_coin = 1
+    for i in [coin_val for coin_val in coinArray if coin_val <= c]:
+      if min_coins_count[c - i] + 1 < coin_count:
+        coin_count = min_coins_count[c - i] + 1
+        latest_coin = i
+    min_coins_count[c] = coin_count
+    min_coins_used[c] = latest_coin
 
-def make_change(coin_val_list, change, known_results):
-  min_coins = change
-  if change in coin_val_list:
-    known_results[change] = 1
-    return 1
-  elif known_results[change] > 0:
-    return known_results[change]
-  else:
-    for i in [c for c in coin_val_list if c <= change]:
-      num_coins = 1 + make_change(coin_val_list, change - i, known_results)
-      if num_coins < min_coins:
-        min_coins = num_coins
-        known_results[change] = min_coins
-  return min_coins
+  min_count = min_coins_count[change]
+  min_used = []
+  for each in coinArray:
+    min_used.append(0)
+  change_iter = change
+  while change_iter > 0:
+    coin = min_coins_used[change_iter]
+    k = 0
+    for j in coinArray:
+      if coin == j:
+        min_used[k] += 1
+      k += 1
 
-print(make_change([1, 3, 4], 11, [0]*64))
+    change_iter = change_iter - coin
+
+  return min_count, min_used
+
+print(changedp([1, 3, 4], 11))
 print(make_change([1, 10, 25, 50], 40, [0]*64))
 print(make_change([1, 5, 10, 25, 50], 75, [0]*100))
 print(make_change([1, 10, 21, 50], 63, [0]*100))
