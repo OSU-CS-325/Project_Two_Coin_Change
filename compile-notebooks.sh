0bf2119642909a2cb30d4142a6391ef9d96eb888
ammiranda <alexandermichaelmiranda@gmail.com>
2016-10-22 14:53:06 -0700

    Created divide_and_conquer notebook file, updated compile-notebooks script for existing py notebooks, added content to question8 notebook

--- compile-notebooks.sh
+++ compile-notebooks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm report/final_report.ipynb 
-python nbmerge.py report/header_page.ipynb > report/final_report.ipynb 
+python nbmerge.py report/header_page.ipynb divide-conquer/divide_and_conquer.ipynb greedy/greedy.ipynb dynamic-programming/dynamic_programming.ipynb > report/final_report.ipynb 
