c1f7582313e1dd595394b06b862bad2bed03e69c
ammiranda <alexandermichaelmiranda@gmail.com>
2016-10-22 16:35:30 -0700

    Added code to generate question 4 plots using slow algorithm

--- run-files/analysis.py
+++ run-files/analysis.py
@@ -41,14 +41,14 @@ numCoinsDP = [0] * len(change)
 numCoinsSlow = [0] * len(change)
 
 for i, amt in enumerate(change):
-	_, numCoinsGreedy[i] = changegreedy(coinArray, amt)
-	_, numCoinsDP[i] = changedp(coinArray, amt)
+  _, numCoinsGreedy[i] = changegreedy(coinArray, amt)
+  _, numCoinsDP[i] = changedp(coinArray, amt)
   _, numCoinsSlow[i] = changeslow(coinArray, amt)
 
 plt.figure(2)
 plt.plot(change, numCoinsGreedy, 'b-', linewidth=2.0, label='Greedy')
 plt.plot(change, numCoinsDP, 'r--', linewidth=2.0, label='DP')
-plt.plot(change, numCoinsSlow, 'g-', linewidth=2.0, label='Slow')
+plt.plot(change, numCoinsSlow, 'g-:', linewidth=2.0, label='Slow')
 plt.legend(loc='upper left')
 plt.ylabel('# of Coins')
 plt.xlabel('Change Amount')
@@ -89,6 +89,46 @@ plt.xlabel('Change Amount')
 plt.grid(True)
 plt.savefig('img/Q4b.png', bbox_inches='tight')
 
+### QUESTION 4 WITH SLOW ###
+coinArray1 = [1, 2, 6, 12, 24, 48, 60] 
+coinArray2 = [1, 6, 13, 37, 150]
+change = range(1, 30)
+
+numCoinsGreedy1 = [0] * len(change)
+numCoinsGreedy2 = [0] * len(change)
+numCoinsSlow1 = [0] * len(change)
+numCoinsSlow2 = [0] * len(change)
+numCoinsDP1 = [0] * len(change)
+numCoinsDP2 = [0] * len(change)
+
+for i, amt in enumerate(change):
+	_, numCoinsGreedy1[i] = changegreedy(coinArray1, amt)
+	_, numCoinsGreedy2[i] = changegreedy(coinArray2, amt)
+  _, numCoinsSlow1[i] = changeslow(coinArray1, amt)
+  _, numCoinsSlow2[i] = changeslow(coinArray2, amt)
+	_, numCoinsDP1[i] = changedp(coinArray1, amt)
+	_, numCoinsDP2[i] = changedp(coinArray2, amt)
+
+plt.figure(5)
+plt.plot(change, numCoinsGreedy1, 'b-', linewidth=2.0, label='Greedy')
+plt.plot(change, numCoinsDP1, 'r--', linewidth=2.0, label='DP')
+plt.plot(change, numCoinsSlow1, 'g-:', linewidth=2.0, label='Slow')
+plt.legend(loc='upper left')
+plt.ylabel('# of Coins')
+plt.xlabel('Change Amount')
+plt.grid(True)
+plt.savefig('img/Q4a_slow.png', bbox_inches='tight')
+
+plt.figure(6)
+plt.plot(change, numCoinsGreedy2, 'b-', linewidth=2.0, label='Greedy')
+plt.plot(change, numCoinsDP2, 'r--', linewidth=2.0, label='DP')
+plt.plot(change, numCoinsSlow2, 'g-:', linewidth=2.0, label='Slow')
+plt.legend(loc='upper left')
+plt.ylabel('# of Coins')
+plt.xlabel('Change Amount')
+plt.grid(True)
+plt.savefig('img/Q4b_slow.png', bbox_inches='tight')
+
 ### QUESTION 5 ###
 coinArray = [1] + range(2, 31, 2)
 change = range(2000, 2201)
@@ -100,7 +140,7 @@ for i, amt in enumerate(change):
 	_, numCoinsGreedy[i] = changegreedy(coinArray, amt)
 	_, numCoinsDP[i] = changedp(coinArray, amt)
 
-plt.figure(5)
+plt.figure(7)
 plt.plot(change, numCoinsGreedy, 'b-', linewidth=2.0, label='Greedy')
 plt.plot(change, numCoinsDP, 'r--', linewidth=2.0, label='DP')
 plt.legend(loc='upper left')
@@ -122,10 +162,10 @@ for i, amt in enumerate(change):
   _, numCoinsDP[i] = changedp(coinArray, amt)
   _, numCoinsSlow[i] = changeslow(coinArray, amt)
 
-plt.figure(6)
+plt.figure(8)
 plt.plot(change, numCoinsGreedy, 'b-', linewidth=2.0, label='Greedy')
 plt.plot(change, numCoinsDP, 'r--', linewidth=2.0, label='DP')
-plt.plot(change, numCoinsSlow, 'g--', linewidth=2.0, label='Slow')
+plt.plot(change, numCoinsSlow, 'g-:', linewidth=2.0, label='Slow')
 plt.legend(loc='upper left')
 plt.ylabel('# of Coins')
 plt.xlabel('Change Amount')
