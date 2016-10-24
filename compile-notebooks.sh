709994437f1ba1534fd5ee41f8cbe4e72b2ab753
Alexander Miranda <alex.miranda@noaa.gov>
2016-10-24 09:00:49 -0700

    Updated compile notebook script to include question 6 and 7

--- compile-notebooks.sh
+++ compile-notebooks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm report/final_report.ipynb 
-python nbmerge.py report/header_page.ipynb divide-conquer/divide_and_conquer.ipynb greedy/greedy.ipynb dynamic-programming/dynamic_programming.ipynb run-files/question3.ipynb run-files/question4.ipynb run-files/question5.ipynb run-files/question8.ipynb > report/final_report.ipynb 
+python nbmerge.py report/header_page.ipynb divide-conquer/divide_and_conquer.ipynb greedy/greedy.ipynb dynamic-programming/dynamic_programming.ipynb run-files/question3.ipynb run-files/question4.ipynb run-files/question5.ipynb run-files/question6.ipynb run-files/question7.ipynb run-files/question8.ipynb > report/final_report.ipynb 
