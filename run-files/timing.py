648818d0d7efd977e4478cfa7266f3018a9846b4
Kyle Guthrie <guthriek@oregonstate.edu>
2016-10-23 20:19:28 -0700

    Change colors, move title, small tweaks to code

--- run-files/timing.py
+++ run-files/timing.py
@@ -16,6 +16,8 @@ from changeslow import changeslow
 from changegreedy import changegreedy
 from changedp import changedp
 
+fitOutput = open('fit_coefficients.txt', 'w')
+
 def Q3():
 	coinArray = [1, 5, 10, 25, 50]
 	# change = range(1000, 100001, 1000)
@@ -42,20 +44,30 @@ def Q3():
 	poptGreedy, pcovGreedy = curve_fit(linearCurve, np.asarray(change), np.asarray(runtimeGreedy))
 	poptDP, pcovDP = curve_fit(linearCurve, np.asarray(change), np.asarray(runtimeDP))
 
+	fitOutput.write("Q3 Greedy Linear Curve Fit: \n")
+	fitOutput.write("Slope = " + str(poptGreedy[0]) + "\n")
+	fitOutput.write("Intercept = " + str(poptGreedy[1]) + "\n")
+	fitOutput.write("\n\n")
+
+	fitOutput.write("Q3 DP Linear Curve Fit: \n")
+	fitOutput.write("Slope = " + str(poptDP[0]) + "\n")
+	fitOutput.write("Intercept = " + str(poptDP[1]) + "\n")
+	fitOutput.write("\n\n")
+
 	fitGreedy = linearCurve(np.asarray(change), poptGreedy[0], poptGreedy[1])
 	fitDP = linearCurve(np.asarray(change), poptDP[0], poptDP[1])
 
 	plt.figure(1)
 	plt.subplot(2, 1, 1)
-	plt.plot(change, runtimeGreedy, 'b.', linewidth=2.0, label='Greedy data')
-	plt.plot(change, fitGreedy, 'k--', linewidth=2.0, label='Curve Fit')
+	plt.plot(change, runtimeGreedy, 'k.', linewidth=2.0, label='Greedy data')
+	plt.plot(change, fitGreedy, 'b--', linewidth=2.0, label='Curve Fit')
 	plt.legend(loc='upper left')
 	plt.ylabel('Avg. Runtime (sec)')
 	plt.title('V = [1, 5, 10, 25, 50]')
 	plt.grid(True)
 	plt.subplot(2, 1, 2)
-	plt.plot(change, runtimeDP, 'r.', linewidth=2.0, label='DP data')
-	plt.plot(change, fitDP, 'k--', linewidth=2.0, label='Curve Fit')
+	plt.plot(change, runtimeDP, 'k.', linewidth=2.0, label='DP data')
+	plt.plot(change, fitDP, 'r--', linewidth=2.0, label='Curve Fit')
 	plt.legend(loc='upper left')
 	plt.ylabel('Avg. Runtime (sec)')
 	plt.xlabel('Change Amount')
