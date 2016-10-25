86deea0a5dfc88c4e2a491675819dcb62c833b49
Michael Stramel <stramelm@flip3.engr.oregonstate.edu>
2016-10-24 21:22:25 -0700

    Adding first trial version of plot for Q7

--- run-files/analysisQ7.py
+++ run-files/analysisQ7.py
@@ -2,7 +2,8 @@ import sys
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import time
+import random
+import datetime
 
 # Import the three change making algorithms
 sys.path.insert(0, "../divide-conquer/")
@@ -14,105 +15,93 @@ from changegreedy import changegreedy
 from changedp import changedp
 
 ### QUESTION 7 ###
-def Q7():
-	coinArray = [1, 5, 10, 25, 50]
-	change = range(2010, 2201)
+def Q7(slow):
 
-	numCoinsGreedy = [0] * len(change)
-	numCoinsDP = [0] * len(change)
+	lenV = []
+	runtimeGreedy = []
+	runtimeDP = []
+	runtimeSlow = []
 
-	runtimeGreedy = [0] * len(change)
-	runtimeDP = [0] * len(change)
+	numExp = 10
+	maxRange = 100
+	if (slow):
+		maxRange = 10 # some much smaller number
 
-	for i, amt in enumerate(change):
-		for j in range(10):
-			t0 = time.clock()
-			_, numCoinsGreedy[i] = changegreedy(coinArray, amt)
-			runtimeGreedy[i] += time.clock() - t0
+	for i in range(1, maxRange): # V can be of length 1 to (maxRange - 1)
+		print "\n------ running V length=" + str(i) + "------"
+		lenV.append(i)
+		print "lenV:", lenV
+		runtimeGreedy.append(0)
+		runtimeDP.append(0)
+		runtimeSlow.append(0)
 
-			t0 = time.clock()
-			_, numCoinsDP[i] = changedp(coinArray, amt)
-			runtimeDP[i] += time.clock() - t0
+		for j in range(numExp): # run numExp experiments for this length of V
+			print "\n  ---- running experiment=" + str(j + 1) + " ----"
+			coinArray = []
 
-		runtimeGreedy[i] /= 10
-		runtimeDP[i] /= 10
+			for k in range(i): # generate V of size i [1, rand, ..., rand, max=1 + 5*(maxRange-2)]
+				if (k == 0):
+					coinArray.append(1)
+				else:
+					randFrom = coinArray[len(coinArray) - 1] + 1
+					randTo = coinArray[len(coinArray) - 1] + 5
+					coinArray.append(random.randint(randFrom, randTo))
+			change = random.randint(1, 30)
+			print "  coinArray:", coinArray				
+			print "  change:", change			
 
-	plt.figure(1)
-	plt.plot(change, numCoinsGreedy, 'b-', linewidth=2.0, label='Greedy')
-	plt.plot(change, numCoinsDP, 'r--', linewidth=2.0, label='DP')
-	plt.legend(loc='upper left')
-	plt.title('V = [1, 5, 10, 25, 50]')
-	plt.ylabel('# of Coins')
-	plt.xlabel('Change Amount')
-	plt.grid(True)
-	plt.savefig('img/Q3.png', bbox_inches='tight')
+			print "  running greedy..."
+			start = datetime.datetime.now()
+			_, _ = changegreedy(coinArray, change)
+			end = datetime.datetime.now()
+			delta = end - start
+			delta = int(delta.total_seconds() * 1000000)
+			print "    " + str(delta)
+			runtimeGreedy[i - 1] += delta
 
-	plt.figure(20)
-	plt.plot(change, runtimeGreedy, 'b-', linewidth=2.0, label='Greedy')
-	plt.plot(change, runtimeDP, 'r--', linewidth=2.0, label='DP')
-	plt.legend(loc='upper left')
-	plt.title('V = [1, 5, 10, 25, 50]')
-	plt.ylabel('Avg. Runtime (sec)')
-	plt.xlabel('Change Amount')
-	plt.grid(True)
-	plt.savefig('img/Q3_runtime.png', bbox_inches='tight')
+			print "  running DP..."
+			start = datetime.datetime.now()
+			_, _ = changedp(coinArray, change)
+			end = datetime.datetime.now()
+			delta = end - start
+			delta = int(delta.total_seconds() * 1000000)
+			print "    " + str(delta)
+			runtimeDP[i - 1] += delta
+			
+			if (slow):
+				print "  running slow..."
+				start = datetime.datetime.now()
+				_, _ = changeslow(coinArray, change)
+				end = datetime.datetime.now()
+				delta = end - start
+				delta = int(delta.total_seconds() * 1000000)
+				print "    " + str(delta)
+				runtimeSlow[i - 1] += delta
 
-### QUESTION 7 WITH SLOW ###
-def Q7slow():
-	coinArray = [1, 5, 10, 25, 50]
-	change = range(1, 30)
-
-	numCoinsGreedy = [0] * len(change)
-	numCoinsDP = [0] * len(change)
-	numCoinsSlow = [0] * len(change)
-
-	runtimeGreedy = [0] * len(change)
-	runtimeDP = [0] * len(change)
-	runtimeSlow = [0] * len(change)
-
-	for i, amt in enumerate(change):
-		for j in range(10):
-			t0 = time.clock()
-			_, numCoinsGreedy[i] = changegreedy(coinArray, amt)
-			runtimeGreedy[i] += time.clock() - t0
-
-			t0 = time.clock()
-			_, numCoinsDP[i] = changedp(coinArray, amt)
-			runtimeDP[i] += time.clock() - t0
-
-			t0 = time.clock()
-			_, numCoinsSlow[i] = changeslow(coinArray, amt)
-			runtimeSlow[i] += time.clock() - t0
-
-		runtimeGreedy[i] /= 10
-		runtimeDP[i] /= 10
-		runtimeSlow[i] /= 10
-
-	plt.figure(2)
-	plt.plot(change, numCoinsGreedy, 'b-', linewidth=2.0, label='Greedy')
-	plt.plot(change, numCoinsDP, 'r--', linewidth=2.0, label='DP')
-	plt.plot(change, numCoinsSlow, 'g-.', linewidth=2.0, label='Slow')
-	plt.legend(loc='upper left')
-	plt.title('V = [1, 5, 10, 25, 50]')
-	plt.ylabel('# of Coins')
-	plt.xlabel('Change Amount')
-	plt.grid(True)
-	plt.savefig('img/Q3_slow.png', bbox_inches='tight')
+		runtimeGreedy[i - 1] /= numExp 
+		runtimeDP[i - 1] /= numExp
+		if (slow):
+			runtimeSlow[i - 1] /= numExp
 
 	plt.figure(21)
-	plt.plot(change, runtimeGreedy, 'b-', linewidth=2.0, label='Greedy')
-	plt.plot(change, runtimeDP, 'r--', linewidth=2.0, label='DP')
-	plt.plot(change, runtimeSlow, 'g-.', linewidth=2.0, label='Slow')
+	plt.plot(lenV, runtimeGreedy, 'b-', linewidth=2.0, label='Greedy')
+	plt.plot(lenV, runtimeDP, 'r--', linewidth=2.0, label='DP')
+	if (slow):
+		plt.plot(lenV, runtimeSlow, 'g-.', linewidth=2.0, label='Slow')
 	plt.legend(loc='upper left')
-	plt.title('V = [1, 5, 10, 25, 50]')
+	plt.title('Runtime vs len(V[]) for randomized V[] and A')
 	plt.ylabel('Avg. Runtime (sec)')
-	plt.xlabel('Change Amount')
+	plt.xlabel('len(V[]')
 	plt.grid(True)
-	plt.savefig('img/Q3slow_runtime.png', bbox_inches='tight')
+	if (slow):
+		plt.savefig('img/Q7slow_runtime.png', bbox_inches='tight')
+	else:
+		plt.savefig('img/Q7_runtime.png', bbox_inches='tight')
+		
 
 def main():
-	Q7()
-	Q7slow()
+	Q7(False)
+	#Q7(True)
 
 
 if __name__ == "__main__":
